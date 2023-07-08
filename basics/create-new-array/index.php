25347bd84761647dbc1500ff7c41e4ca838e1d68
FatihBARACKILIC <fatihb469@gmail.com>
2023-07-08 20:45:14 +0300

    Create New Array

--- basics/create-new-array/index.php
+++ basics/create-new-array/index.php
@@ -1,24 +1,42 @@
-<pre>
-<?php
+<!DOCTYPE html>
+<html lang="tr">
 
-$planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "", "", NULL];
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create New Array</title>
+</head>
 
-function createNewArray($array, $itemCount): array
-{
-    $newArray = [];
-    $array = array_filter($array, function ($value) {
-        return $value !== Null && $value !== "";
-    });
-    $arrayKey = array_rand($array, $itemCount);
-    foreach ($arrayKey as $key) {
-        $newArray[] = $array[$key];
-    }
-    return $newArray;
-}
+<body>
+    <pre>
+        <?php
+        $planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "", "", NULL];
 
-print_r(createNewArray($planets, 2));
-print_r(createNewArray($planets, 3));
-print_r(createNewArray($planets, 4));
-print_r(createNewArray($planets, 5));
-?>
-</pre>
+        function createNewArray($array, $itemCount): array
+        {
+            $newArray = [];
+            $array = array_filter($array, function ($value) {
+                return $value !== Null && $value !== "";
+            });
+            if ($itemCount >= count($array))
+                return [
+                    "Error" => "\$itemCount($itemCount) cannot be bigger than array length!"
+                ];
+            $arrayKey = array_rand($array, $itemCount);
+            foreach ($arrayKey as $key) {
+                $newArray[] = $array[$key];
+            }
+            return $newArray;
+        }
+
+        print_r(createNewArray($planets, 2));
+        print_r(createNewArray($planets, 3));
+        print_r(createNewArray($planets, 4));
+        print_r(createNewArray($planets, 7));
+        print_r(createNewArray($planets, 17));
+        ?>
+    </pre>
+
+</body>
+
+</html>
