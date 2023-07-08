631a6fd8076ee4f5a02876aa0c6bfa8617df5881
FatihBARACKILIC <fatihb469@gmail.com>
2023-07-08 20:45:05 +0300

    Index Page

---
+++ basics/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="tr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1><?= "Hello, PHP!" ?></h1>
+    <ul>
+        <li>
+            <a href="./triangle/">
+                Triangle
+            </a>
+        </li>
+        <li>
+            <a href="./create-new-array/">
+                Create New Array
+            </a>
+        </li>
+    </ul>
+</body>
+
+</html>
