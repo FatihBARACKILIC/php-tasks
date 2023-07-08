4af3d7a1978627c3260ae63bde0df02dd7785df5
FatihBARACKILIC <fatihb469@gmail.com>
2023-07-08 20:45:19 +0300

    Triangle

--- basics/triangle/index.php
+++ basics/triangle/index.php
@@ -1,14 +1,33 @@
-<?php
+<!DOCTYPE html>
+<html lang="tr">
 
-function triangle($lineCount){
-    for($i = 0; $i <= $lineCount; $i++){
-        $k = 0;
-        while($k <= $i){
-            echo "0 ";
-            $k++;
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Triangle</title>
+</head>
+
+<body>
+
+    <?php
+    function triangle($lineCount)
+    {
+        for ($i = 0; $i <= $lineCount; $i++) {
+            $k = 0;
+            while ($k <= $i) {
+                echo "0 ";
+                $k++;
+            }
+            echo "<br />";
         }
-        echo "<br>";
     }
-}
 
-triangle(15);
+    triangle(15);
+    echo "<hr />";
+    triangle(5);
+    echo "<hr />";
+    triangle(10);
+    ?>
+</body>
+
+</html>
